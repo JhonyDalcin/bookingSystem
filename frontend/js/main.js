8e46ed39f234bd64c51fcca7a66c3bc9185d0c4b
Luis Vieira <luisernani87@gmail.com>
2023-04-20 16:03:03 -0300

    work done, next step personalizate it as my taste

---
+++ frontend/js/main.js
@@ -0,0 +1,17 @@
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+function blurFocusFunc(){
+    let parent = this.parentNode.parentNode;
+    if(this.value == ''){
+        parent.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunc);
+    input.addEventListener('blur', blurFocusFunc);
+});
